cc78f9e79d5bff6e87b3424d19cebf7cb2561090
abdoulc <abdoulrazak1996@gmail.com>
2021-10-19 21:05:01 +0200

    add Spring Cloud Config server

--- order-service/src/main/java/com/abdel/os/api/service/OrderService.java
+++ order-service/src/main/java/com/abdel/os/api/service/OrderService.java
@@ -6,17 +6,25 @@ import com.abdel.os.api.common.TransactionResponse;
 import com.abdel.os.api.entity.Order;
 import com.abdel.os.api.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@RefreshScope
 public class OrderService {
 
     @Autowired
     private OrderRepository orderRepository;
 
     @Autowired
+    @Lazy
     private RestTemplate template;
+
+    @Value("${microservice.payment-service.endpoints.endpoint.uri}")
+    private String ENDPOINT_PAYMENT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest transactionRequest){
         String response="";
@@ -24,8 +32,7 @@ public class OrderService {
         Payment payment = transactionRequest.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
-
-        Payment paymentResponse = template.postForObject("http://PAYMENT-SERVICE/payment/doPayment", payment,Payment.class);
+        Payment paymentResponse = template.postForObject(ENDPOINT_PAYMENT_URL, payment,Payment.class);
         response=paymentResponse.getPaymentStatus().equals("success")?"payment successfull":"there is failure in payment api";
         orderRepository.save(order);
         return  new TransactionResponse(order,paymentResponse.getAmount(),paymentResponse.getTransactionId(), response);
